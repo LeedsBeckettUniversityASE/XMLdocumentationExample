c14c68bd360a3b470e696740cf2b919d5bd6e6b1
Duncan Mullier <D.Mullier@leedsbeckett.ac.uk>
2020-10-09 10:11:08 +0100

    initial commit

---
+++ TestingDocumentation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TestingDocumentation
+{
+    /// <summary>
+    /// class Program is to show XML comments
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// entry point to porhram
+        /// </summary>
+        /// <param name="args">Command line, array of Strings</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        /// <summary>
+        /// Compares two integers number1 with number2 and outputs the biggest, zero if the same
+        /// </summary>
+        /// <param name="number1">first number to compare</param>
+        /// <param name="number2">second number to compare</param>
+        /// <returns>biggest integer or zero if the same</returns>
+        int compare(int number1, int number2)
+        {
+            if (number1 > number2)
+                return number1;
+            else if (number2 > number1)
+                return number2;
+            else return 0;
+        }
+    }
+}
